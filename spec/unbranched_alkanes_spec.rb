32807407810744aa382059feaa6e29de8719522e
Tom Rossen <rossentj@acm.org>
2013-01-26 14:37:31 -0600

    automate SMILES generation in spec

--- spec/unbranched_alkanes_spec.rb
+++ spec/unbranched_alkanes_spec.rb
@@ -4,27 +4,33 @@ require_relative '../src/smiles_to_iupac.rb'
 describe SmilesToIupacTranslator do
   it "translates H-suppressed methane representation from SMILES to IUPAC" do
     translator = SmilesToIupacTranslator.new
-    iupac = translator.translate "C"
+    iupac = translator.translate makeUnbranched 1
     iupac.should eq "methane"
   end
   it "translates H-suppressed ethane representation from SMILES to IUPAC" do
     translator = SmilesToIupacTranslator.new
-    iupac = translator.translate "CC"
+    iupac = translator.translate makeUnbranched 2
     iupac.should eq "ethane"
   end
   it "translates H-suppressed nonane representation from SMILES to IUPAC" do
     translator = SmilesToIupacTranslator.new
-    iupac = translator.translate "CCCCCCCCC"
+    iupac = translator.translate makeUnbranched 9
     iupac.should eq "nonane"
   end
   it "translates H-suppressed decane representation from SMILES to IUPAC" do
     translator = SmilesToIupacTranslator.new
-    iupac = translator.translate "CCCCCCCCCC"
+    iupac = translator.translate makeUnbranched 10
     iupac.should eq "decane"
   end
   it "translates H-suppressed nonadecane representation from SMILES to IUPAC" do
     translator = SmilesToIupacTranslator.new
-    iupac = translator.translate "CCCCCCCCCCCCCCCCCCC"
+    iupac = translator.translate makeUnbranched 19
     iupac.should eq "nonadecane"
   end
+  def makeUnbranched carbonCount
+    c = ""
+    1.upto(carbonCount) {c << "C"}
+    c
+  end
+
 end
